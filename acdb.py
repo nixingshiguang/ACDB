78b3ce6f29d08cd82df7218be86150205db503f9
逆行时光 <94590099+nixingshiguang@users.noreply.github.com>
2024-03-08 20:39:46 +0800

    完善逻辑
    开箱即用、可随时更改图片API和切换间隔

--- acdb.py
+++ acdb.py
@@ -5,32 +5,21 @@ import ctypes
 import time
 import threading
 
+# 默认的URL和间隔时间
+url = "https://imgapi.160621.xyz/random.php"
+interval = 60
+
+# 计数起始值
+count = 1
+
+# 设置壁纸
 def set_wallpaper(image_path):
-    """
-    设置指定图像路径为桌面壁纸。
-
-    参数:
-    image_path: 字符串，指定的图像文件路径。
-
-    返回值:
-    无
-    """
     SPI_SETDESKWALLPAPER = 20  # 定义设置桌面壁纸的系统参数标识符
     # 使用ctypes调用Windows API来设置桌面壁纸
     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 3)
 
-# 计数
-count = 1
+# 下载并设置壁纸
 def download_image(url):
-    """
-    从指定的URL下载图像并设置为桌面壁纸。
-    
-    参数:
-    url: str - 图像的URL地址。
-    
-    返回值:
-    无
-    """
     global count  # 声明count为全局变量
     print(f"第{count}次切换壁纸...")
     
@@ -60,57 +49,43 @@ def download_image(url):
     print("切换壁纸成功！")
     count += 1
 
-def on_submit():
-    """
-    处理提交操作，根据输入的URL和间隔时间，启动一个新线程定期更改桌面壁纸。
-    
-    无参数
-    无返回值
-    """
-    # 获取用户输入的URL和间隔时间
-    url = url_entry.get()
-    interval = int(interval_entry.get())
-    
+# 更换壁纸
+def wallpaper_change():
+    global url, interval
     def wallpaper_changer():
-        """
-        在一个独立的线程中，周期性地从指定URL下载图片并设置为桌面壁纸。
-        
-        无参数
-        无返回值
-        """
         while True:  # 不断循环，定期执行下载和设置壁纸的操作
             download_image(url)  # 下载图片
             time.sleep(interval)  # 按照设定间隔休眠，单位为秒
     
-    # 创建一个新线程并启动，目标函数为wallpaper_changer
     thread = threading.Thread(target=wallpaper_changer)
     thread.start()
+
+wallpaper_change()  # 启动壁纸切换线程
+
+# 确认按钮事件
+def on_submit():
+    # 从交互界面获取用户输入的URL和间隔时间
+    global url, interval
+    url = url_entry.get()
+    interval = int(interval_entry.get())
 
 # 创建GUI界面
 root = tk.Tk()
 # 标题
 root.title("ACDB 壁纸自动更换器")
-
 # 图片API_url标签
 url_label = tk.Label(root, text="图片API_URL:")
 url_label.pack()  # 将标签添加到根窗口
 url_entry = tk.Entry(root, width=50) # 创建输入框，设置宽度为50字符
-url_entry.insert(0, "https://imgapi.160621.xyz/random.php")
 url_entry.pack()  # 将输入框添加到根窗口
-
 # 时间间隔标签
 interval_label = tk.Label(root, text="壁纸切换间隔 (秒):")
 interval_label.pack()
 interval_entry = tk.Entry(root)
-interval_entry.insert(0, "30")
 interval_entry.pack()
-
 # 确认按钮
 submit_button = tk.Button(root, text="确认", command=on_submit)
 submit_button.pack()
 
-# 运行根窗口的事件循环
-#
-# 该函数没有参数。
-# 也没有返回值，因为它会一直运行，直到窗口被关闭或程序终止。
 root.mainloop()
+
